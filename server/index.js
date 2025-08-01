f6a18e827c82b9b90bb5319301e9c3f4c4d75b12
Biplab jena <jbiplab80@gmail.com>
2025-08-01 21:07:12 +0530

    Build course progress page

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ import userRoute from "./routes/user.route.js";
 import courseRoute from "./routes/course.route.js"
 import mediaRoute from "./routes/media.route.js"
 import purchaseRoute from "./routes/purchaseCourse.route.js"
+import courseProgressRoute from "./routes/courseProgress.route.js"
 
 dotenv.config({});
 //call database connection here
@@ -29,6 +30,7 @@ app.use("/api/v1/media", mediaRoute);
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/course", courseRoute);
 app.use("/api/v1/purchase", purchaseRoute);
+app.use("/api/v1/progress", courseProgressRoute);
 
 
 app.listen(PORT, () => {
