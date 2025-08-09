cd67550467bad8ce7834011e8586d887694c2a52
Biplab jena <jbiplab80@gmail.com>
2025-08-09 21:35:01 +0530

    UPDAte

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials:true
 }));
 
